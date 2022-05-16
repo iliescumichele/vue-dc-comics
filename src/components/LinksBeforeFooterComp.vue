e07dd3cd0a8bd030abd5af860ff3a347b4de19ff
iliescumichele <iliescumichele@gmail.com>
2022-05-16 20:52:45 +0200

    Layout -> links before footer

--- src/components/LinksBeforeFooterComp.vue
+++ src/components/LinksBeforeFooterComp.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-    
+    <div class="links-before-footer">
+        <div class="container">
+            <div class="link">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="">DIGITAL COMICS</a>
+            </div>
+            <div class="link">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                <a href="">DC MERCHANDISE</a>
+
+            </div>
+            <div class="link">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <a href="">SUBSCRIPTION</a>
+                
+            </div>
+            <div class="link">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <a href="">COMIC SHOP LOCATOR</a>
+
+            </div>
+            <div class="link">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <a href="">DC POWER VISA</a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'LinksBEforeFooterComp'
+    name: 'LinksBeforeFooterComp'
 }
 </script>
 
 <style lang="scss" scoped>
-    
+    .links-before-footer{
+        height: 150px;
+        background-color: #0081f9;
+
+        .container{
+            display: flex;
+            justify-content: space-evenly;
+        }
+    }
+
+    .link{
+        display: flex;
+        align-items: center;
+
+        a{
+            color: white;
+            margin: 15px;
+        }
+
+        img{
+            height: 40%;
+        }
+    }
 </style>
